f9880dc8cdffb4f18cec6c9f6d7d40a310ee4b84
WyattPH224 <wyatt.harris224@gmail.com>
2023-06-11 18:01:44 -0700

    Level and gameplay added

---
+++ README.md
@@ -0,0 +1,10 @@
+Audio 2 kinds:
+Visual 2 kinds:
+    image based sprite for player
+    tilemap for level
+Motion:
+    physics based jumping
+Progression:
+Prefabs:
+    Level Scene subclasses
+    Level design saved in json file 
--- script.js
+++ script.js
@@ -26,6 +26,13 @@ class Menu extends Phaser.Scene {
 }
 
 let upScoreButton = Phaser.GameObjects.Text;
+let keyA;
+let keyD;
+let keyW;
+let keyS;
+let keySpace;
+let spaceDown;
+let power;
 class Level extends Phaser.Scene {
     constructor(key, name, num) {
         super(key);
@@ -35,14 +42,56 @@ class Level extends Phaser.Scene {
     
     create(){
 
+        /*
         upScoreButton = this.add.text(100, 500, `Increase score`, {font: '64px Arial', fill: '#ffffff'})
         .setInteractive()
         .on('pointerdown', () => score++ );
-
+        */
+        spaceDown = false;
+        power = 0;
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.onStart();
     }
     update(){
-        upScoreButton.setText(`Increase score: ${score}`);
+        //upScoreButton.setText(`Increase score: ${score}`);
+        //movement left right
+        if(keyA.isDown && this.player.body.onFloor()){
+            this.player.setVelocityX(-100);
+        }
+        else if (keyA.isDown) {
+            this.player.setVelocityX(-300);
+            console.log("A down in air");
+        }
+        else if (keyD.isDown && this.player.body.onFloor()) {  
+            this.player.setVelocityX(100);
+        } 
+        else if (keyA.isDown) {
+            this.player.setVelocityX(300);
+        }
+        else if(keyA.isUp && keyD.isUp){
+            this.player.setVelocityX(0);
+        }
+
+        
+        if (keySpace.isDown) {
+            this.player.setVelocityX(0);
+            spaceDown = true;
+            if(power < 100) {
+                power+=2;
+            }
+
+            console.log("space down " + power);
+        } 
+        if (spaceDown && this.player.body.onFloor() && keySpace.isUp) {
+            this.player.setVelocityY(-250 - (power * 4));
+            power = 0;
+            spaceDown = false;
+            console.log("space up");
+        }
     }
 
     onStart(){
@@ -63,13 +112,30 @@ class Level1 extends Level {
     }
 
     preload(){
-
+        // At last image must be loaded with its JSON
+        this.load.image('player', 'assets/Player.png');
+        this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+         // Load the export Tiled JSON
+         this.load.tilemapTiledJSON('map', 'assets/levels/level1.json');
     }
 
     onStart(){
-        const jumpButton = this.add.text(100, 100, 'Jump up to next level', {font: '64px Arial', fill: '#ffffff'})
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('level2') );
+        //const jumpButton = this.add.text(100, 100, 'Jump up to next level', {font: '64px Arial', fill: '#ffffff'})
+        //.setInteractive()
+       // .on('pointerdown', () => this.scene.start('level2') );
+
+        const map = this.make.tilemap({key: 'map'});                                //load map
+        const tileset = map.addTilesetImage('core_gameplay_platformer', 'tiles');   //load tileset for map
+        const platforms = map.createLayer('Platforms', tileset, 0, 0);        //create platforms layer
+        platforms.setCollisionByExclusion(-1, true);                                //set collision for platforms layer
+
+
+        //create player
+        this.player = this.physics.add.sprite(100, 1000, 'player');
+        this.player.setBounce(0.1);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, platforms);
+
 
        
 
@@ -194,7 +260,7 @@ let config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
-        height: 1080
+        height: 1280
     },
     backgroundColor: 0x000000,
     scene: [Menu, Level1, Level2, Level3, EndCutscene, Settings, Credits],
